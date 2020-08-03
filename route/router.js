56a58661c1ffe63b3f351e93e512d9d108ef1686
shariful583 <shariful.cse583@gmail.com>
2020-08-03 20:53:31 +0600

    router updated

--- route/router.js
+++ route/router.js
@@ -6,20 +6,22 @@ const auth = require('../auth/auth');
 
 
 const router = express.Router();
+
+// User
+
 // product
-router.post('/admin/product/create',productController.createProduct);
-router.get('/product/show',productController.getProduct);
-router.get('/product/:id',productController.Product);
-router.delete('/product/:id',productController.deleteProduct);
-
-
+router.get('/api/product/show',productController.getProduct);
+router.get('/api/product/:id',productController.Product);
+router.delete('/api/product/:id',productController.deleteProduct);
 // Category
-router.get('/category/show',auth,CategoryController.getCategory);
-router.post('/admin/category/create',auth,CategoryController.create);
-
+router.get('/api/category/show',CategoryController.getCategory);
 
 
 // admin
-router.get('/admin/login/:token',auth,adminController.login);
+// router.get('/admin/login/:token',auth,adminController.login);
+router.post('/api/admin/category/create',auth,CategoryController.create);
+router.post('/api/admin/product/create',productController.createProduct);
+router.delete('/api/admin/category/create',auth,CategoryController.create);
+
 
 module.exports = router;
